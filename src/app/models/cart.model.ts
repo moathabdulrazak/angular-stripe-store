90fd3333a11214112574aed4bd8ac3b1d73700f0
moath abdulrazak <moathabdulrazak12@gmail.com>
2023-03-11 07:05:48 -0700

    cart styled and displaying data

--- src/app/models/cart.model.ts
+++ src/app/models/cart.model.ts
@@ -1,4 +1,3 @@
-
 export interface Cart{
   items: Array<CartItem>;
 }
--- src/app/pages/cart/cart.component.ts
+++ src/app/pages/cart/cart.component.ts
@@ -7,30 +7,41 @@ import { Cart, CartItem } from 'src/app/models/cart.model';
 })
 export class CartComponent implements OnInit {
 
-cart: Cart = {items: [{
-  product: 'https://via.placeholder.com/150',
-  name: 'sneakers',
-  price: 150,
-  quantity: 1,
-  id: 1,
-}]}
+  cart: Cart = { items: [    {
+    product: 'https://via.placeholder.com/150x150',
+    name: 'Product 1',
+    price: 10.99,
+    quantity: 2,
+    id: 1,
+  },
+  {
+    product: 'https://via.placeholder.com/150x150',
+    name: 'Product 2',
+    price: 5.99,
+    quantity: 1,
+    id: 2,
+  },
+] };
+  dataSource: Array<CartItem> = [];
+  displayedColumns: Array<string> = [
+    'product',
+    'name',
+    'price',
+    'quantity',
+    'total',
+    'action',
+  ];
 
-dataSource: Array<CartItem> = [];
-displayedColumns: Array <string> = [
-'product',
-// 'name',
-// 'price',
-// 'quantity',
-// 'total',
-// 'action',
-];
-
- constructor () {}
-
+  constructor() {}
 
   ngOnInit(): void {
     this.dataSource = this.cart.items;
   }
 
-
+  getTotal(items: Array<CartItem>): string {
+    const total = items
+      .map((item) => item.price * item.quantity)
+      .reduce((prev, current) => prev + current, 0);
+    return total.toFixed(2);
+  }
 }
