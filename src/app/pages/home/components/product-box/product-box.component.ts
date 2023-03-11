00b248bd59b648dab70e62d8df0634be63b8b358
moath abdulrazak <moathabdulrazak12@gmail.com>
2023-03-11 05:57:52 -0700

    product-box completed. homepage done

---
+++ src/app/pages/home/components/product-box/product-box.component.ts
@@ -0,0 +1,16 @@
+import { Component, OnInit, Input } from '@angular/core';
+
+@Component({
+  selector: 'app-product-box',
+  templateUrl: 'product-box.component.html' ,
+})
+export class ProductBoxComponent implements OnInit {
+
+@Input() fullWidthMode = false
+
+  constructor() {}
+
+  ngOnInit(): void {
+    
+  }
+}
