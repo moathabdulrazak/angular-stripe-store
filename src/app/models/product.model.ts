51e9b044af7e7f0c226eaf16352244a3eb90db53
moath abdulrazak <moathabdulrazak12@gmail.com>
2023-03-11 08:50:06 -0700

    adding to cart log works with snackbar

--- src/app/models/product.model.ts
+++ src/app/models/product.model.ts
@@ -1,10 +1,8 @@
-
-
-export interface Product{
-  id: number;
-  name: string;
-  price: number;
-  category: string;
-  description: string;
-  image: string;
-}
+  export interface Product {
+    id: number;
+    name: string;
+    price: number;
+    category: string;
+    description: string;
+    image: string;
+  }
--- src/app/services/cart.service.ts
+++ src/app/services/cart.service.ts
@@ -1,6 +1,7 @@
 import { Injectable } from '@angular/core';
+import { MatSnackBar } from '@angular/material/snack-bar';
 import { BehaviorSubject } from 'rxjs';
-import { Cart } from '../models/cart.model';
+import { Cart, CartItem } from '../models/cart.model';
 
 @Injectable({
   providedIn: 'root'
@@ -8,5 +9,23 @@ import { Cart } from '../models/cart.model';
 export class CartService {
 cart = new BehaviorSubject<Cart>({items: []})
 
-  constructor() { }
+  constructor(private _snackBar: MatSnackBar) { }
+
+
+  addToCart(item: CartItem): void{
+const items  = [...this.cart.value.items]
+
+const itemInCart = items.find((_item) => _item.id === item.id)
+if(itemInCart){
+  itemInCart.quantity +=1;
+}
+else{
+  items.push(item);
+}
+
+this.cart.next({items});
+this._snackBar.open('1 item added to cart.', 'Keep Shopping', {duration: 3000} )
+console.log(this.cart.value, "value");
+
+  }
 }
