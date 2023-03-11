9a946f212b1a49a1778c99b0acf490d9adc5cffb
moath abdulrazak <moathabdulrazak12@gmail.com>
2023-03-11 06:18:16 -0700

    setting up cart foundation

--- src/app/pages/cart/cart.component.ts
+++ src/app/pages/cart/cart.component.ts
@@ -1,5 +1,5 @@
 import { Component, OnInit } from '@angular/core';
-import { Cart } from 'src/app/models/cart.model';
+import { Cart, CartItem } from 'src/app/models/cart.model';
 
 @Component({
   selector: 'app-cart',
@@ -7,13 +7,29 @@ import { Cart } from 'src/app/models/cart.model';
 })
 export class CartComponent implements OnInit {
 
-cart: Cart = {items: []}
+cart: Cart = {items: [{
+  product: 'https://via.placeholder.com/150',
+  name: 'sneakers',
+  price: 150,
+  quantity: 1,
+  id: 1,
+}]}
+
+dataSource: Array<CartItem> = [];
+displayedColumns: Array <string> = [
+'product',
+'name',
+'price',
+'quantity',
+'total',
+'action',
+];
 
  constructor () {}
 
 
   ngOnInit(): void {
-    
+    this.dataSource = this.cart.items;
   }
 
 
