d2d4b5bc0b9e585b0b741b0e99e9e537eea1c2c4
moath abdulrazak <moathabdulrazak12@gmail.com>
2023-03-12 03:19:43 -0600

    categories displaying

--- src/app/pages/home/components/filters/filters.component.ts
+++ src/app/pages/home/components/filters/filters.component.ts
@@ -1,20 +1,35 @@
-import { Component, OnInit, Output, EventEmitter} from '@angular/core';
+import { Component, OnInit, Output, EventEmitter, OnDestroy} from '@angular/core';
+import { Subscription } from 'rxjs';
+import { StoreService } from 'src/app/services/store.service';
 
 @Component({
   selector: 'app-filters',
   templateUrl: 'filters.component.html',
 })
-export class FiltersComponent implements OnInit {
+export class FiltersComponent implements OnInit, OnDestroy {
 @Output() showCategory = new EventEmitter<string>();
-  categories = ['shoes', 'sports'];
+categoriesSubscription: Subscription | undefined;
+categories: string[] = [];
 
-  constructor() {}
+  constructor(private storeService: StoreService) {}
 
-  ngOnInit(): void {}
+  ngOnInit(): void {
+  this.categoriesSubscription = this.storeService.getAllCategories().subscribe((response) => {
+  this.categories = response;
+});
+
+  }
 
   onShowCategory(category: string): void {
     this.showCategory.emit(category);
     console.log("category", category);
     
+  }
+
+
+  ngOnDestroy(): void {
+    if(this.categoriesSubscription){
+this.categoriesSubscription.unsubscribe();
+    }
   }
 }
